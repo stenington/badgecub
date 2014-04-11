706a7665835f8f0c5d8c38638b296edc545d10f4
Mike Larsson <mikelarssonftw@gmail.com>
2014-04-11 10:03:26 -0400

    Allow static files to be hosted externally

--- index.js
+++ index.js
@@ -50,17 +50,28 @@ var app = express();
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('templates'));
 env.express(app);
 
+var staticDir = path.join(__dirname, '/static');
+var staticRoot = '/static';
+
+app.use(function (req, res, next) {
+  res.locals.static = function static (staticPath) {
+    var root = config('STATIC_ROOT', staticRoot);
+    return path.join(root, staticPath);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(sass.middleware({
   src: path.join(__dirname, "./styles"),
-  dest: path.join(__dirname, "./static/css"),
+  dest: path.join(staticDir, "css"),
   prefix: "/static/css",
   debug: DEBUG
 }));
-app.use('/static', express.static(path.join(__dirname, '/static')));
+app.use(staticRoot, express.static(staticDir));
 
 function isAction (action) {
   return function (req, res, next) {
